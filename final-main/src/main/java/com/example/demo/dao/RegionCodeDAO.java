682783b038122b179a21ae95ae44007f2244f4a8
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-04-08 16:40:36 +0900

    RegionCodeDAO [주선]

---
+++ final-main/src/main/java/com/example/demo/dao/RegionCodeDAO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.RegionCode;
+import com.example.demo.entity.Users;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegionCodeDAO extends JpaRepository<RegionCode, Integer> {
+
+}
