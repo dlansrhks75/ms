4abbd1d676ea09955ca03268bdf1a88e6dd26505
dlansrhks75 <dlansrhks75@naver.com>
2024-04-11 12:04:25 +0900

    changePwd - 비밀번호 수정 작업 [문관]

--- final-main/src/main/java/com/example/demo/service/UsersService.java
+++ final-main/src/main/java/com/example/demo/service/UsersService.java
@@ -20,4 +20,8 @@ public class UsersService {
 	public int updateInfo(String u_name, String u_email, String u_phone, String u_nickname, String u_fname, String rno, int uno) {
 		return dao.updateInfo(u_name, u_email, u_phone, u_nickname, u_fname, rno, uno);
 	}
+	
+	public void updatePwd(String newPwd, int uno) {
+		dao.updatePwd(newPwd, uno);
+	}
 }
